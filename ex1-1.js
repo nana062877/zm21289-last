39fc256f6fae6617563d2923bc9a1d3c1dfc5eac
nana062877 <131091948+nana062877@users.noreply.github.com>
2023-12-19 10:20:46 +0900

    Create ex1-1.js

---
+++ ex1-1.js
@@ -0,0 +1,9 @@
+function imgchange1(){
+  document.getElementById('osi').src =
+    './image/rapu.jpg';
+}
+
+function imgchange2(){
+  document.getElementById('osi').src =
+    './image/pino.jpg';
+}
