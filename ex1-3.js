9b9e05521626c95cbc5a09f96c08f632c9c0a4b9
nana062877 <131091948+nana062877@users.noreply.github.com>
2023-12-22 10:58:11 +0900

    Create ex1-3.js

---
+++ ex1-3.js
@@ -0,0 +1,7 @@
+function rapu2(){
+  if(document.getElementById('rapu').value == 'ラプンツェル'){
+    alert('あたり！');
+  }else if(document.getElementById('rapu').value = 'ラプンツェ'){
+    alert('はずれ！');
+  }
+}
